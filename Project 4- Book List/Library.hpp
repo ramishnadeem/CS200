ef1f2c966628540a90aa172a5f77f801163f13f5
Ramish Nadeem <nadeemramish98@gmail.com>
2017-07-24 01:06:10 -0500

    Add files via upload

---
+++ Project 4- Book List/Library.hpp
@@ -0,0 +1,31 @@
+#ifndef _LIBRARY_HPP
+#define _LIBRARY_HPP
+#include "Book.hpp"
+
+
+class Library
+{
+private:
+    Book* m_bookList;
+    int m_arraySize;
+    int m_bookCount;
+    
+public:
+    Library();
+    ~Library();
+    void DeallocateMemory();
+    void AllocateMemory(int size);
+    void ResizeArray();
+    int GetArraySize();
+    int GetBookCount();
+    bool IsArrayFull();
+    void Run();
+    void MainMenu();
+    void NewBook();
+    void UpdateBook();
+    void DisplayBooksWithIndex();
+    void ViewAllBooks();
+    void ViewStats();
+};
+
+#endif
